d16efecf629ba0f4cbcd6275962e667176595493
jbulling <48367982+jbulling@users.noreply.github.com>
2019-11-21 08:14:20 -0600

    Asteroids game 8:14AM

--- src/AsteroidsGame/AsteroidsGame.java
+++ src/AsteroidsGame/AsteroidsGame.java
@@ -1,4 +1,24 @@
 package AsteroidsGame;
 
+import comp127graphics.CanvasWindow;
+
 public class AsteroidsGame {
+    CanvasWindow canvas;
+    AsteroidsManager manager;
+    private final int CANVAS_WIDTH = 600;
+    private final int CANVAS_HEIGHT = 800;
+
+    public AsteroidsGame(){
+        canvas = new CanvasWindow("Asteroids!", CANVAS_WIDTH, CANVAS_HEIGHT);
+        manager = new AsteroidsManager();
+        run();
+    }
+
+    public void run(){
+       //TODO: Finish this.
+    }
+
+
+
+
 }

51ee4541bb1a9f4ee43f5c7532e76bbdec461702
zoeschopick <zschopic@macalester.edu>
2019-11-21 10:25:23 -0600

    ship stuff

--- src/AsteroidsGame/Ship.java
+++ src/AsteroidsGame/Ship.java
@@ -1,4 +1,51 @@
 package AsteroidsGame;
 
-public class Ship {
+
+import comp127graphics.Fillable;
+import comp127graphics.Line;
+
+import java.awt.*;
+
+
+public class Ship implements Fillable {
+
+    Line line1;
+    Line line2;
+    Line line3;
+    private Paint fillColor;
+    private boolean isFilled;
+
+
+    public Ship(int x1, int y1, int x2, int y2, int x3, int y3){
+        line1 = new Line(x1,y1,x2,y2);
+        line2 = new Line(x2, y2, x3, y3);
+        line3 = new Line(x3, y3, x1, y1);
+        fillColor = Color.RED;
+        isFilled = true;
+
+    }
+
+
+
+
+
+    @Override
+    public Paint getFillColor() {
+        return null;
+    }
+
+    @Override
+    public void setFillColor(Paint fillColor) {
+
+    }
+
+    @Override
+    public boolean isFilled() {
+        return false;
+    }
+
+    @Override
+    public void setFilled(boolean filled) {
+
+    }
 }

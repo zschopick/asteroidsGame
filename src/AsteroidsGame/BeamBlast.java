3782875a23739778ef0467708246577deaa7eb02
hayleyhadges <hhadges@macalester.edu>
2019-11-21 10:20:10 -0600

    BeamBlast

--- src/AsteroidsGame/BeamBlast.java
+++ src/AsteroidsGame/BeamBlast.java
@@ -1,10 +1,12 @@
 package AsteroidsGame;
 
+import comp127graphics.CanvasWindow;
 import comp127graphics.Ellipse;
 
 import java.awt.*;
 
 public class BeamBlast extends Ellipse {
+    CanvasWindow canvas;
     private Ship shipShape;
     private double x;
     private double y;
@@ -14,19 +16,18 @@ public class BeamBlast extends Ellipse {
      * @param x      position
      * @param y      position
      */
-    public BeamBlast(double x, double y, double radius, Ship shipShape) {
+    public BeamBlast(double x, double y, double radius, Ship shipShape, CanvasWindow canvas) {
         super(x, y, radius*2, radius*2);
         this.x = x;
         this.y = y;
+        this.radius = radius;
         this.setFillColor(Color.green);
-
-    }
-
-    private void setStart(){
+        this.shipShape = shipShape;
+        this.canvas = canvas;
         this.setPosition(shipShape.getShipFront());
     }
 
-    private double calculateAngle(){
+    private double calculateSlope(){
         double x0 = shipShape.x0;
         double y0 = shipShape.y0;
         double x3 = shipShape.getShipBottomX();
@@ -35,7 +36,25 @@ public class BeamBlast extends Ellipse {
         return (x3-x0)/(y3-y0);
     }
 
-    void updatePosition() {
-        //shipShape.x0;
+    private boolean xDirectionPositive(){
+        double x0 = shipShape.x0;
+        double x3 = shipShape.getShipBottomX();
+        return x3-x0 > 0;
+    }
+
+    void updateBeamPosition() {
+        double slope = calculateSlope();
+        if (xDirectionPositive()){
+            x++;
+        }
+        else {
+            x--;
+        }
+        y = slope * x + shipShape.y0;
+        this.setPosition(x,y);
+    }
+
+    private void removeBeam(){
+        canvas.remove(this);
     }
 }

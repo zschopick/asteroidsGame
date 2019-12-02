a98359d71b90ad3c6e2fad0ca50725f9ad4fb363
jbulling <48367982+jbulling@users.noreply.github.com>
2019-12-01 17:58:35 -0700

    Asteroids Move randomly

--- src/AsteroidsGame/Asteroids.java
+++ src/AsteroidsGame/Asteroids.java
@@ -5,13 +5,15 @@ import comp127graphics.CanvasWindow;
 import comp127graphics.Ellipse;
 
 import java.awt.*;
+import java.util.Random;
 
 public class Asteroids extends Ellipse{
     private final double asteroidRadius = 100;
-    private double dX = .1;
-    private double dY = .1;
+    private double dX;
+    private double dY;
     private double xPos;
     private double yPos;
+    private Random rand;
 
     /**
      * Creates an ellipse whose upper left is at (x,y), and which has the specified width and height.
@@ -23,6 +25,9 @@ public class Asteroids extends Ellipse{
     public Asteroids(double x, double y) {
         super(x, y, 100, 100);
         setStrokeColor(Color.white);
+        rand = new Random();
+        setdX(rand.nextDouble());
+        setdY(rand.nextDouble());
 
     }
 
@@ -40,11 +45,27 @@ public class Asteroids extends Ellipse{
             setPosition(canvas.getWidth(), getY());
         }
 
-        xPos = getX() + dX;
-        yPos = getY() + dY;
+        xPos = getX() + getdX();
+        yPos = getY() + getdY();
         setPosition(xPos, yPos);
     }
 
+    private double getdX() {
+        return dX;
+    }
 
 
+    public void setdX(double dX) {
+        this.dX = dX;
+    }
+
+    public void setdY(double dY){
+        this.dY = dY;
+    }
+
+    public double getdY() {
+        return dY;
+    }
 }
+
+
